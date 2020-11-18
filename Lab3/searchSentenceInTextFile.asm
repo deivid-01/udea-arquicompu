566aa30116e2da9d04e8dc417aab24be85e9d9a7
deivid-01 <datcx@hotmail.com>
2020-11-17 21:33:51 -0500

    Lab 3: Number of aparitions done

--- Lab3/searchSentenceInTextFile.asm
+++ Lab3/searchSentenceInTextFile.asm
@@ -2,10 +2,11 @@
 	textFile: .asciiz  "Kate Winslet guarda su Oscar en el cuarto de baño para que sus invitados puedan sostenerlo e improvisar sus propios discursos de agradecimiento sin sentirse observados."
 	messageInit: .asciiz " Enter sentence to search: "
 	sentence: .space 20
-	finalMessage: .asciiz "\nThe number of aparitions of  "
+	sentenceMessage: .asciiz "\nSentence: "
+	aparitionsMessage: .asciiz "Number of aparitions: "
 	endMessage: .asciiz "\nEnd of the program "
 	messageSuccess: .asciiz "Sentence has been found in textFile"
-	newLine: .asciiz "\n"
+	newLine: .asciiz "is : "
 .text
 		
 	main: 
@@ -49,6 +50,8 @@
 		addi $t0,$zero,0 # initial index value for textFile
 		addi $t1,$zero,0 # intial index value  for sentence
 		
+		addi $t6,$zero,0 #Counter of ocurrences
+		
 		while:
 					
 			bgt $t0,$t4,exit	#if( $to>$t4) | t4 cointains the lenght of textFile
@@ -73,7 +76,19 @@
 					
 		exit:
 			li $v0,4
-			la $a0,endMessage
+			la $a0,sentenceMessage
+			syscall
+			
+			li $v0,4
+			la $a0,sentence
+			syscall
+			
+			li $v0,4
+			la $a0,aparitionsMessage
+			syscall
+			
+			li $v0, 1
+			move $a0,$t6
 			syscall
 			
 			li $v0, 10
@@ -140,12 +155,14 @@
 	
 					
 	foundSentence:	
-		li $v0, 4
-		la $a0, messageSuccess
-		syscall
 		
-		li $v0, 10
-		syscall		
+		addi $t6,$t6,1
+		
+		addi $t0,$t0,1
+		addi $t1,$zero,1
+		
+		
+		j while	
 	increaseIndexes:
 		addi $t0,$t0,1 # incremenet t0, t0 is the index
 		addi $t1,$t1,1 # incremenet t1, t1 is the index
