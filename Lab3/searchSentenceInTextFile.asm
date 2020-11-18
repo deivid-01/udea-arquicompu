30983ad6561cb8d022b089b8ef4bf9d4da6dcb7d
deivid-01 <datcx@hotmail.com>
2020-11-17 19:57:39 -0500

    get lenght of textFile and input

--- Lab3/searchSentenceInTextFile.asm
+++ Lab3/searchSentenceInTextFile.asm
@@ -1,16 +1,22 @@
 .data
-	textFile: .asciiz  "lunabrabola"
+	textFile: .asciiz  "Kate Winslet guarda su Oscar en el cuarto de baño para que sus invitados puedan sostenerlo e improvisar sus propios discursos de agradecimiento sin sentirse observados."
 	messageInit: .asciiz " Enter sentence to search: "
 	sentence: .space 20
 	finalMessage: .asciiz "\nThe number of aparitions of  "
 	endMessage: .asciiz "\nEnd of the program "
 	messageSuccess: .asciiz "Sentence has been found in textFile"
+	newLine: .asciiz "\n"
 .text
 		
 	main: 
 		
 		jal printMessageInit 
 		jal readSentence	#Read user input
+		
+		jal getLenghtTextFile # $t4=textFile.Lenght
+		jal getLenghtSentence # $t5=sentence.Lenght
+		
+		
 		jal searchSentence
 	
 	
@@ -45,7 +51,7 @@
 		
 		while:
 					
-			bgt $t0,11,exit	#Condition if(t>11)exit
+			bgt $t0,$t4,exit	#if( $to>$t4) | t4 cointains the lenght of textFile
 			#Indexing strings############################
 			addu $a2,$a3,$t0   
 			lbu $a0,($a2)		# a2 = textFile[t0]			
@@ -72,12 +78,60 @@
 			
 			li $v0, 10
 			syscall
+			
+			
+	getLenghtTextFile:
+	
+		
+		la $a1,textFile 
+		addi $t4,$zero,0 #t4->Lenght of textFile
+		
+		whileText:
+		 
+	
+			addu $a2,$a1,$t4   
+			lbu $a0,($a2)	
+		 	
+		 	beq $a0,$zero,exitText
+		 			 	
+		 	addi $t4,$t4,1
+		 	
+		 	j whileText
+		   
+		 exitText: 
+		 	sub $t4,$t4,1
+  			jr $ra	
+		
+		
+	getLenghtSentence:
+	
+		
+		la $a1,sentence 
+		addi $t5,$zero,0 #t4->Lenght of textFile
+		
+		whileSentence:
+		 
+	
+			addu $a2,$a1,$t5   
+			lbu $a0,($a2)	
+		 	
+		 	beq $a0,10,exitSentence
+			
+		 	addi $t5,$t5,1
+		 	
+		 	j whileSentence
+		   
+		 exitSentence: 
+		 	sub $t5,$t5,1
+  			jr $ra	
+		
+				
 
 ######### EXTRA METHODS		
 
 	equalChar:
-		beq $t1,3,foundSentence	 ###IMPORTANT: Set input sentence lenght here
-		bne $t1,3,increaseIndexes ###IMPORTANT: Set input sentence lenght here
+		beq $t1,$t5,foundSentence	 ###IMPORTANT: Set input sentence lenght here
+		bne $t1,$t5,increaseIndexes ###IMPORTANT: Set input sentence lenght here
 
 		
 	equalCharNot:
